aa89c0fffab9f67ac64639113b4c62fca638210c
zooloska <sirmakrstovska@gmail.com>
2023-12-07 20:49:41 +0100

    JS HW

---
+++ SEDC-G3-Sirma-Homeworks/JS Homeworks/JS_Homework_1/java_script/script.js
@@ -0,0 +1,48 @@
+// Task_1
+let phones = prompt('Please enter a number of phones:');
+let price = 119.95;
+let taxRate = price * 0.05;
+let resultTask1 = (price + taxRate)*phones;
+console.log ('The resul  from Task_1 is:', resultTask1);
+
+
+// Task_2
+const conversionValue = 60;
+let minutes = 9;
+let seconds = minutes * conversionValue;
+console.log ("The result from Task_2 is: ", seconds);
+
+//Task_3
+let num = 256;
+let num1 = ++num;
+console.log ('The result from Task_3 is:  ', num1);
+
+
+// Task_4
+let base = 357;
+let height = 756;
+let resultTask4 = 357*756/2;
+console.log ('The result from Task_4 is:', resultTask4);
+// console.log (357 * 756 / 2);
+
+//Task_5
+let length = 154;
+let width = 444;
+let result2 = length + width;
+let resultTask5 = result2 * 2;
+console.log ('The result from Task_5 is:   ', resultTask5);
+
+//Task_6
+let radius= 69;
+let circleArea = Math.PI * radius ** 2;
+console.log ('The result from Task_6 is:', circleArea);
+
+
+
+
+
+
+
+
+
+
