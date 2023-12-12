57694ae218665c796c32789b8a20128fd6133f33
zooloska <sirmakrstovska@gmail.com>
2023-12-12 22:51:25 +0100

    update- 2 zadaci

---
+++ SEDC-G3-Sirma-Homeworks/JS Homeworks/Homework-JS-3/script/script.js
@@ -0,0 +1,45 @@
+console.log(" -------- HOMEWORK PART 1 -------- ");
+
+function getTypeParameter (type) {
+    let typeResult = typeof(type);
+    return typeResult;
+}
+
+console.log(getTypeParameter('text')); 
+console.log(getTypeParameter(true)); 
+console.log(getTypeParameter()); 
+console.log(getTypeParameter(null)); 
+console.log(getTypeParameter(33)); 
+
+
+
+
+
+
+
+console.log(" -------- HOMEWORK PART 2 -------- ");
+
+function humanToDogYears (value) {
+    result = value * 7;
+    return result;
+}
+
+function dogToHumanYears (userInput) {
+    result = userInput / 7;
+    return  result;
+}
+
+
+let humanToDogYearsUserInput = prompt('Enter human years, to be converted to dog years.');
+let resultDog = humanToDogYears(humanToDogYearsUserInput);
+console.log (resultDog);
+
+let dogToHumanYearsInput = prompt('Enter dog years, to be converted to human years.');
+let resultHuman = dogToHumanYears(dogToHumanYearsInput);
+console.log (resultHuman);
+
+
+
+
+
+
