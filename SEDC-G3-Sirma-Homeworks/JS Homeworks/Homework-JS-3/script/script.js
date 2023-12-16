b339a3ee3aca98705dfe8bb27a2d34c2600e0843
zooloska <sirmakrstovska@gmail.com>
2023-12-16 20:53:56 +0100

    Class-03 Homework

--- SEDC-G3-Sirma-Homeworks/JS Homeworks/Homework-JS-3/script/script.js
+++ SEDC-G3-Sirma-Homeworks/JS Homeworks/Homework-JS-3/script/script.js
@@ -42,4 +42,30 @@ console.log (resultHuman);
 
 
 
+console.log ("---- HOMEWORK PART 3 ------");
+
+function giveCash (atm) {
+    const  moneyFix = 5000;
+    if (atm < moneyFix) {
+        let money = moneyFix - atm;
+        let result = `Izvadivte ${atm} denari. Momentalnata sostojba na Vashata smetka e ${money}.`;
+        return result ;
+    }
+    else if (atm > moneyFix) {
+        let result = "Nemate dovolno pari!";
+        return result;
+    }     
+
+    else {
+        let result = "Ve molime vnesete iznos!";
+        return result;
+    }
+}
+
+let giveCashInput = prompt ("Kolku pari sakate da podignete?");
+let parsedInput = parseInt (giveCashInput);
+let cashResult = giveCash (parsedInput);
+console.log (cashResult);
+
+  
 
