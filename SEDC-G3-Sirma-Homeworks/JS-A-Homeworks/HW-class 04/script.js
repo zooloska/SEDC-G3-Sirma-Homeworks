f44a16bda150c438784aa482adbcbbe2497c77c1
zooloska <sirmakrstovska@gmail.com>
2024-01-24 16:44:27 +0100

    hw-04

---
+++ SEDC-G3-Sirma-Homeworks/JS-A-Homeworks/HW-class 04/script.js
@@ -0,0 +1,49 @@
+console.log('--Excercise - 1--');
+console.log('--Exercise - 2--');
+
+
+
+
+
+console.log('--Exercise - 3--');
+
+let arrayOfNumbers = [1, 3, 4, 5, 6, 7];
+
+let sqareNumbers = (array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let square = array [i] * array [i];
+        result.push(square);        
+    }
+    return result;
+}
+
+let resultArray = sqareNumbers(arrayOfNumbers);
+console.log(resultArray);
+
+
+
+console.log('--Exercise - 4--');
+
+
+(() => {
+    let number = !n;
+    for (n = 0; index < number.length; i++) {
+        console.log(number); 
+        result *= n;
+         n--;
+    }
+    return result;
+ });
+
+//  console.log(result);
+ 
+
+//  (function() {
+//     document.getElementById('myElement').innerHTML = 'Content set by a self-invoking function.';
+// })();
+
+console.log('--Exercise - 5--');
+console.log('--Exercise - 6--');
+console.log('--Exercise - 7--');
+
